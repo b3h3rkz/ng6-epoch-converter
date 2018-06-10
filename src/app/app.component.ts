ed44d2153284e9f6c6b47811023a9424597245f0
Bernard Parah <barjebernard@gmail.com>
2018-06-10 16:02:46 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Epoch  Time Converter';
+  epoch: any;
+  current_time: Date;
+  human_format: any;
+  human_format_utc: any;
+
+  constructor() {
+    this.current_time =  new Date();
+    this.epoch = moment().format('X');
+  }
+
+  convertEpoch(epoch_time) {
+        this.human_format = moment.unix(epoch_time);
+        this.human_format_utc = moment.unix(epoch_time).utc();
+  }
+
+  convertToEpoch(time) {
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import * as moment from 'moment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
